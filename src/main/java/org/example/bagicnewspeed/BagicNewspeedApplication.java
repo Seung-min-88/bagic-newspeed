fff2aa6579350e695f4cc53862b34067680db283
이승민 <iseungmin@iseungmin-ui-MacBookPro.local>
2025-03-10 15:28:43 +0900

    first commit

---
+++ src/main/java/org/example/bagicnewspeed/BagicNewspeedApplication.java
@@ -0,0 +1,13 @@
+package org.example.bagicnewspeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BagicNewspeedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BagicNewspeedApplication.class, args);
+    }
+
+}
---
+++ src/main/java/org/example/bagicnewspeed/domain/profile/service/ProfileService.java
@@ -0,0 +1,9 @@
+package org.example.bagicnewspeed.domain.profile.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class ProfileService {
+}
